9fc450c1bcef153ee07e139e6cba976608c062b4
erimcolin <gabymcolin@gmail.com>
2018-01-27 01:43:59 -0600

    primera prueba de login usando firebase

---
+++ js/main.js
@@ -0,0 +1,10 @@
+// Initialize Firebase
+  var config = {
+    apiKey: "AIzaSyAzS0COGKf_1hearowkt0TGMGPIdtz3C0Q",
+    authDomain: "producto-final-11726.firebaseapp.com",
+    databaseURL: "https://producto-final-11726.firebaseio.com",
+    projectId: "producto-final-11726",
+    storageBucket: "",
+    messagingSenderId: "314770465477"
+  };
+  firebase.initializeApp(config);
