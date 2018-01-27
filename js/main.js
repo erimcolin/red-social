465bcce7af3341f9f0cbc5841b6802c892655c52
erimcolin <gabymcolin@gmail.com>
2018-01-27 02:39:50 -0600

    funciones en main.js

--- js/main.js
+++ js/main.js
@@ -8,3 +8,37 @@
     messagingSenderId: "314770465477"
   };
   firebase.initializeApp(config);
+
+$('#botonGoogle').click(function(){
+  authGoogle();
+});
+
+function authGoogle(){
+  var provider = new firebase.auth.GoogleAuthProvider();
+  autentication(provider);
+}
+
+function autentication(provider) {
+  firebase.auth().signInWithPopup(provider).then(function(result) {
+  // This gives you a Google Access Token. You can use it to access the Google API.
+  var token = result.credential.accessToken;
+  // The signed-in user info.
+  var user = result.user;
+  console.log(result);
+  // ...
+}).catch(function(error) {
+  console.log(error);
+  // Handle Errors here.
+  var errorCode = error.code;
+  console.log(errorCode);
+  var errorMessage = error.message;
+  console.log(errorMessage);
+  // The email of the user's account used.
+  var email = error.email;
+  console.log(email);
+  // The firebase.auth.AuthCredential type that was used.
+  var credential = error.credential;
+  console.log(credential);
+  // ...
+});
+}
