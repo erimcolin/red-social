f056c21d5856e9bdecab7a44670dea2872e2a5d5
AnnKrn <anakrn.carreon@gmail.com>
2018-01-30 19:48:13 -0600

    funcion search

--- js/data.js
+++ js/data.js
@@ -1,65 +1,65 @@
-var data = {
+var data = [
   {
     'name': 'Adriana Juárez',
-    'img': '..assets/images/img1.jpg',
-    'title': 'atardecer'
+    'img': 'assets/images/img1.jpg',
+    'title': 'atardecer',
     'category': 'bosque',
     'description': 'hermoso atarceder en medio del bosque'
-  }
+  },
   {
     'name': 'Carla Sánchez',
-    'img': '..assets/images/img2.jpg',
-    'title': 'Neblina'
+    'img': 'assets/images/img2.jpg',
+    'title': 'Neblina',
     'category': 'bosque',
     'description': 'Qué intenso!'
-  }
+  },
   {
     'name': 'Camilo Morocho',
-    'img': '..assets/images/img3.jpg',
-    'title': 'Montañas'
+    'img': 'assets/images/img3.jpg',
+    'title': 'Montañas',
     'category': 'montañas',
     'description': 'En la inmensidad de las montañas'
-  }
+  },
   {
     'name': 'Juana Segura',
-    'img': '..assets/images/img4.jpg',
-    'title': 'Atarceder'
+    'img': 'assets/images/img4.jpg',
+    'title': 'Atarceder',
     'category': 'playa',
     'description': 'Un paseo por la playa y una vista inolvidable'
-  }
+  },
   {
     'name': 'Isabel Márquez',
-    'img': '..assets/images/img5.jpg',
-    'title': 'Nubes'
+    'img': 'assets/images/img5.jpg',
+    'title': 'Nubes',
     'category': 'bosque',
     'description': 'está muy alto aquí'
-  }
+  },
   {
     'name': 'Audacia Montreal',
-    'img': '..assets/images/img6.jpg',
-    'title': 'Lo logramos'
+    'img': 'assets/images/img6.jpg',
+    'title': 'Lo logramos',
     'category': 'montañas',
     'description': 'Y ahora cómo me bajo'
-  }
+  },
   {
     'name': 'Sofía Cuadraro',
-    'img': '..assets/images/img7.jpg',
-    'title': 'Hermosa vista'
+    'img': 'assets/images/img7.jpg',
+    'title': 'Hermosa vista',
     'category': 'montañas',
     'description': 'Aquí nomás'
-  }
+  },
   {
     'name': 'Joséin Hernandez',
-    'img': '..assets/images/img8.jpg',
-    'title': 'Lago'
+    'img': 'assets/images/img8.jpg',
+    'title': 'Lago',
     'category': 'bosque',
     'description': 'Vamo a pescar'
-  }
+  },
   {
     'name': 'Pepe Astuto',
-    'img': '..assets/images/img10.jpg',
-    'title': 'Anochecer'
+    'img': 'assets/images/img10.jpg',
+    'title': 'Anochecer',
     'category': 'ciudad',
     'description': 'Aquí bien cosmopolitan'
   }
-}
+]
